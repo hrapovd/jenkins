cb83554888d064a5ffb02b3a4448398bbc23dd56
Dmitry Khrapov <hrapovd@gmail.com>
2020-08-12 04:41:06 +0300

    fix wget error

--- packer/jenkins-master/setup.sh
+++ packer/jenkins-master/setup.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
 echo "Install Jenkins stable release"
+useradd -d /var/lib/jenkins -s /bin/bash jenkins
 yum remove -y java
 yum install -y java-1.8.0-openjdk
-wget -O /etc/yum.repos.d/jenkins.repo http://pkg.jenkins-ci.org/redhat-stable/jenkins.repo
+curl -o /etc/yum.repos.d/jenkins.repo -L http://pkg.jenkins-ci.org/redhat-stable/jenkins.repo
 rpm --import https://jenkins-ci.org/redhat/jenkins-ci.org.key
 yum install -y jenkins
 chkconfig jenkins on
